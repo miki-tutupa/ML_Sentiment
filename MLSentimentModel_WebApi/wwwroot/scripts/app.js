953535dc9ba98083e4e09928465a4066beb26f5d
Miki Corral <miki@shopworld.es>
2022-10-10 23:28:15 +0200

    vue working

--- MLSentimentModel_WebApi/wwwroot/scripts/app.js
+++ MLSentimentModel_WebApi/wwwroot/scripts/app.js
@@ -1,22 +1,39 @@
 ﻿const app = Vue.createApp({
     data() {
         return {
-            info: null
+            messageToPredict: null,
+            sentiment: null,
+            noToxicScore: null,
+            toxicScore: null,
+            sentimentColor: null,
+            predictedLabel: 0
         }
     },
     methods: {
-        async getData() {
-            try {
-                let response = await fetch("https://sentimentator.azurewebsites.net/predict");
-                this.info = await response.json();;
-            } catch (error) {
-                console.log(error);
-            }
+        initialState() {
+            this.sentiment = null
         },
-    },
-    created() {
-        this.getData();
-    },
+        initialStateTotal() {
+            this.sentiment = null,
+            this.messageToPredict = null
+        },
+        predictMessage() {
+            // Simple POST request with a JSON body using fetch
+            const requestOptions = {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ comment: this.messageToPredict })
+            };
+            fetch("https://sentimentator.azurewebsites.net/predict", requestOptions)
+                .then(response => response.json())
+                .then(data => (
+                    this.sentiment = data.predictedResult,
+                    this.noToxicScore = data.score[0],
+                    this.toxicScore = data.score[1],
+                    this.sentimentColor = data.predictedLabel ? 'red' : 'green'
+                ));
+        }
+    }
 });
 
 app.mount("#hero");
